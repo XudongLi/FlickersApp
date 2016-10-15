1a58c46d7793d8255284acd4a9877bc09de780f7
XudongLi <xudongli@uw.edu>
2016-10-15 14:20:13 -0700

    follow the workthrough video to add the basic functionality there

--- app/src/main/java/com/lixudong/android/flicks/MainActivity.java
+++ app/src/main/java/com/lixudong/android/flicks/MainActivity.java
@@ -2,12 +2,63 @@ package com.lixudong.android.flicks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.lixudong.android.flicks.adapters.MovieArrayAdapter;
+import com.lixudong.android.flicks.models.Movie;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String SAMPLE_URL = "https://api.themoviedb.org/3/movie/now_playing?" +
+            "api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+    ArrayList<Movie> movies;
+    MovieArrayAdapter movieAdapter;
+    ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvItems = (ListView) findViewById(R.id.lvMovies);
+        movies = new ArrayList<>();
+        movieAdapter = new MovieArrayAdapter(this, movies);
+        lvItems.setAdapter(movieAdapter);
+        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+
+        asyncHttpClient.get(SAMPLE_URL, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                JSONArray movieJsonResults = null;
+
+                try {
+                    movieJsonResults = response.getJSONArray("results");
+                    movies.addAll(Movie.fromJSONArray(movieJsonResults));
+                    movieAdapter.notifyDataSetChanged();
+                    Log.d("DEBUG", movieJsonResults.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
     }
+
+
 }
